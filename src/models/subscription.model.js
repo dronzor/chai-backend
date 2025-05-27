92f49fa7af11cd582790684040904dcd06a6e755
Avijit Dey <adey2860@gmail.com>
2025-05-28 00:21:21 +0530

    subscription model for backend project

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose,{Schema} from "mongoose"
+
+const subscriptionSchema= new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,//one who subscribing
+        ref:"User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId,//one whom to "subscriber" is subscribing
+        ref:"User"
+    }
+},
+{
+    timestamps:true
+})
+
+
+
+export const Subscription= mongoose.model("Subscription",subscriptionSchema)
